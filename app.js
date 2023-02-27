298d0526e8f43c314c34cc2988536b0a95c688f2
Vrushab Kudchi <kudchivrushab@gmail.com>
2023-02-27 18:20:37 +0530

    added designed

--- app.js
+++ app.js
@@ -5,16 +5,9 @@ const http = require('http');
 // importing router which was created in route folder
 const route = require('./routes/route');
 
-const hostname = '45.93.139.125';
 // express setup
 const app = express();
 
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hiiiii\n');
-});
 
 //static folder for css and images
 app.use(express.static("public"));
@@ -31,7 +24,7 @@ app.get('/', (req, res) => {
 })
 
 //setting Up Server
-const port = 80;
+const port = 3000;
 
 app.listen(port, function (req, res) {
     console.log("http://localhost:" + port);
